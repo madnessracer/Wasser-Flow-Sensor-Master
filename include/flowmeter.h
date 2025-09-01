f90b00bdcc058c9cb3c3974df5db80cbeb53afc1
Wagner <madnessracing@outlook.com>
2025-09-01 13:57:22 +0200

    modified:   include/flowmeter.h 	modified:   src/main.cpp

--- include/flowmeter.h
+++ include/flowmeter.h
@@ -10,66 +10,83 @@
 #define PULSES_PER_LITER 450.0
 const uint8_t FLOW_SENSOR_PINS[NUM_SENSORS] = {2, 3, 4, 5};
 
-
-class FlowMeterManager {
+class FlowMeterManager
+{
 public:
-    FlowMeterManager() : fram() {
-        for (int i = 0; i < NUM_SENSORS; i++) {
+    FlowMeterManager() : fram()
+    {
+        for (int i = 0; i < NUM_SENSORS; i++)
+        {
             pulseCount[i] = 0;
             flowRate[i] = 0.0;
             totalLiters[i] = 0.0;
         }
     }
-    void begin() {
+    void begin()
+    {
         fram.begin(0x50);
         loadFromFRAM();
-        for (int i = 0; i < NUM_SENSORS; i++) {
+        for (int i = 0; i < NUM_SENSORS; i++)
+        {
             pinMode(FLOW_SENSOR_PINS[i], INPUT_PULLUP);
         }
     }
-    void handleInterrupt(uint8_t idx) {
+    void handleInterrupt(uint8_t idx)
+    {
         pulseCount[idx]++;
     }
-    void timerUpdate() {
-        for (int i = 0; i < NUM_SENSORS; i++) {
+    void timerUpdate()
+    {
+        for (int i = 0; i < NUM_SENSORS; i++)
+        {
             flowRate[i] = (pulseCount[i] / PULSES_PER_LITER) * 60.0;
             totalLiters[i] += (pulseCount[i] / PULSES_PER_LITER);
             pulseCount[i] = 0;
         }
         saveToFRAM();
     }
-    void loadFromFRAM() {
-        for (int i = 0; i < NUM_SENSORS; i++) {
+    void loadFromFRAM()
+    {
+        for (int i = 0; i < NUM_SENSORS; i++)
+        {
             uint32_t addr = i * sizeof(float);
             float val = 0.0;
-            fram.read(addr, (uint8_t*)&val, sizeof(float));
+            fram.read(addr, (uint8_t *)&val, sizeof(float));
             totalLiters[i] = val;
         }
     }
-    void saveToFRAM() {
-        for (int i = 0; i < NUM_SENSORS; i++) {
+    void saveToFRAM()
+    {
+        for (int i = 0; i < NUM_SENSORS; i++)
+        {
             uint32_t addr = i * sizeof(float);
-            fram.write(addr, (uint8_t*)&totalLiters[i], sizeof(float));
+            fram.write(addr, (uint8_t *)&totalLiters[i], sizeof(float));
         }
     }
-    float getFlowRate(uint8_t idx) {
+    float getFlowRate(uint8_t idx)
+    {
         return flowRate[idx];
     }
-    float getTotalLiters(uint8_t idx) {
+    float getTotalLiters(uint8_t idx)
+    {
         return totalLiters[idx];
     }
-    void reset() {
-        for (int i = 0; i < NUM_SENSORS; i++) {
+    void reset()
+    {
+        for (int i = 0; i < NUM_SENSORS; i++)
+        {
             totalLiters[i] = 0.0;
             saveToFRAM();
         }
     }
-    void printSerialValues() {
+    void printSerialValues()
+    {
         extern portMUX_TYPE timerMux;
         portENTER_CRITICAL(&timerMux);
-        for (int i = 0; i < NUM_SENSORS; i++) {
+        for (int i = 0; i < NUM_SENSORS; i++)
+        {
             Serial.print("Sensor ");
-            Serial.print(i+1);
+            Serial.print(i + 1);
             Serial.print(": Durchflussrate: ");
             Serial.print(getFlowRate(i), 2);
             Serial.print(" L/min, Gesamt: ");
@@ -79,6 +96,7 @@ public:
         Serial.println("----------------------");
         portEXIT_CRITICAL(&timerMux);
     }
+
 private:
     volatile uint32_t pulseCount[NUM_SENSORS];
     volatile float flowRate[NUM_SENSORS];
--- src/main.cpp
+++ src/main.cpp
@@ -13,28 +13,31 @@ void IRAM_ATTR flowSensorISR1() { flowMeterManager.handleInterrupt(1); }
 void IRAM_ATTR flowSensorISR2() { flowMeterManager.handleInterrupt(2); }
 void IRAM_ATTR flowSensorISR3() { flowMeterManager.handleInterrupt(3); }
 
-void IRAM_ATTR onTimer() {
+void IRAM_ATTR onTimer()
+{
     portENTER_CRITICAL_ISR(&timerMux);
     flowMeterManager.timerUpdate();
     portEXIT_CRITICAL_ISR(&timerMux);
 }
 
-void setup() {
+void setup()
+{
     Serial.begin(9600);
     flowMeterManager.begin();
+
     attachInterrupt(digitalPinToInterrupt(FLOW_SENSOR_PINS[0]), flowSensorISR0, FALLING);
     attachInterrupt(digitalPinToInterrupt(FLOW_SENSOR_PINS[1]), flowSensorISR1, FALLING);
     attachInterrupt(digitalPinToInterrupt(FLOW_SENSOR_PINS[2]), flowSensorISR2, FALLING);
     attachInterrupt(digitalPinToInterrupt(FLOW_SENSOR_PINS[3]), flowSensorISR3, FALLING);
-    // Hardware-Timer für 1 Sekunde (1 Hz) konfigurieren
-    timer = timerBegin(0, 80, true); // Timer 0, Prescaler 80 (1us Takt)
+
+    timer = timerBegin(0, 80, true);
     timerAttachInterrupt(timer, &onTimer, true);
-    timerAlarmWrite(timer, 1000000, true); // 1.000.000us = 1s
+    timerAlarmWrite(timer, 1000000, true);
     timerAlarmEnable(timer);
 }
 
-void loop() {
+void loop()
+{
     flowMeterManager.printSerialValues();
     delay(1000); // Ausgabe jede Sekunde
 }
-
